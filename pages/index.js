b1961358666acdbd332a1eedd935a7d0dd5de03b
shawnyy18 <shawnbank18@gmail.com>
2024-08-21 13:18:13 +0800

    try

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,15 @@ export default function Home() {
         <h1>Future Cybersecurity Professional</h1>
         <p> an irregular I.T student of University of the Assumption who has a passion for tech stuff, especially the cybersecurity field. My favorite music is more on RNB and HipHop. For foods, I like spicy dishes.</p>
         <p>
+        </p>
         <h2>Hobbies</h2>
+
           <ul>
             <li><p>Watching Educational Videos</p></li>
             <li><p>Travelling</p></li>
             <li><p>Reading</p></li>
           </ul>
-        </p>
+        
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
