97dc17005e3d45e4d180f187c21d0e9373371e0e
shawnyy18 <shawnbank18@gmail.com>
2024-08-22 11:19:19 +0800

    Updated content.

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,16 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
+        <hr></hr>
         <h1>Future Cybersecurity Professional</h1>
-        <p> an irregular I.T student of University of the Assumption who has a passion for tech stuff, especially the cybersecurity field. My favorite music is more on RNB and HipHop. For foods, I like spicy dishes.</p>
+        <p> an irregular I.T student of University of the Assumption who has a passion for tech stuff, especially the cybersecurity field. My favorite music is more on RNB and HipHop. For foods, I like spicy dishes.  </p>
         <p>
         </p>
+        
+
+          <p>I took this course because my patience is so high when it comes to some computer or software problems. That's why I think I will excel in this industry. I want to explore the field of hacking and cybersecurity and I think I will try my best to get the CompTIA Security+ for more opportunities.
+      </p>
+      <hr></hr>
         <h2>Hobbies</h2>
 
           <ul>
@@ -20,7 +26,15 @@ export default function Home() {
             <li><p>Travelling</p></li>
             <li><p>Reading</p></li>
           </ul>
-        
+          <hr></hr>
+          <h2>Technologies I Use</h2>
+          <ul>
+            <li><p>HTML</p></li>
+            <li><p>CSS</p></li>
+            <li><p>JavaScript</p></li>
+            <li><p>VSCode</p></li>
+          </ul>
+
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
